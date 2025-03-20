e1baaeb5143f93a6404c823470caa0025d7313df
Andreas Farre <farre@mozilla.com>
2025-03-20 16:19:40 +0100

    Add support for templates and different merge-bases.

--- README.md
+++ README.md
@@ -9,4 +9,17 @@ ac_add_options MOZ_BUILD_HOOK=$(/path/to/autohooks.sh)
 ```
 
 to your mozconfig. This will use `bookmarks/central` as merge-base.
-If a different merge-base is needed call `autohooks.sh <base-commit>` instead.
+
+```
+hooks -h
+Usage: hooks [-b <merge-base>] [-t template] [-h]
+  -h               Show this help
+  -t FILE          Add template to hooks file
+  -b <MERGE-BASE>  Use <MERGE-BASE> as merge-base
+```
+
+If a different merge-base is needed call `autohooks.sh -b <MERGE-BASE>` instead.
+
+Use `autohooks.sh -t FILE` to include the contents of FILE as prefix of the hooks file.
+
+This can of course be combined to `autohooks.sh -t FILE -b <MERGE-BASE>`.
--- autohooks.sh
+++ autohooks.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/bash
-base=$(git merge-base HEAD ${1-bookmarks/central} 2>/dev/null)
-directories=$(for file in $(git diff --name-only $base HEAD)
+while getopts "hb:t:" option
+do
+  case $option in
+    b) mergebase=$OPTARG
+    ;;
+    t) template=$OPTARG
+    ;;
+    h) echo "Usage: $(basename $0) [-h] [-b <merge-base>] [-t template]"
+       echo "  -h               Show this help"
+       echo "  -t FILE          Add FILE as template for hooks file"
+       echo "  -b <MERGE_BASE>  Use <MERGE-BASE> as merge-base"
+       exit 0
+    ;;
+  esac
+done
+base=$(git merge-base HEAD ${mergebase-bookmarks/central} 2>/dev/null)
+directories=$(for file in $(git diff --name-only $base HEAD 2>/dev/null)
 do
   directory=$(dirname $file)
   if [ -f "${directory}/moz.build" ]
@@ -9,6 +24,10 @@ do
   fi
 done | sort -u)
 temp=$(mktemp)
+if [ -n "${template}" ]
+then
+  cat "${template}" 2>/dev/null > $temp
+fi
 for directory in $directories
 do
   echo "if RELATIVEDIR.startswith(\"${directory}\"):" >> $temp
@@ -16,12 +35,10 @@ do
 done
 tmpdir=${TMPDIR-/tmp}
 hooks=$(ls ${tmpdir}/*.${base} 2>/dev/null | awk '{print $1}')
-if [ -z "${hooks}" ] || [ ! diff -q $hooks $temp &>/dev/null ]
+if [ -z "${hooks}" ] || ! diff -q $hooks $temp &>/dev/null
 then
   rm ${tmpdir}/*.${base} 2>/dev/null
   hooks=$(mktemp --suffix .${base})
-  echo $hooks
   mv $temp $hooks
 fi
-
 echo $hooks
